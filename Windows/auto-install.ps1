8bfdbd4f57555fdecdc4de99ac4d91fe6edcef2f
huffstler <houghstler@gmail.com>
2022-05-02 20:23:24 -0400

    updated install script to use winget import.

---
+++ Windows/auto-install.ps1
@@ -0,0 +1,5 @@
+$foo = New-TemporaryFile
+
+Invoke-RestMethod -Uri "https://raw.githubusercontent.com/huffstler/RiceBowl/master/Windows/winget-install.json" | Set-Content $foo
+
+winget import -i $foo
