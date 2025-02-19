e908028c4f9ee24dc77f7ba38498a677d3623dcf
Pat Hough <phough@jwpepper.com>
2025-02-19 07:01:34 -0500

    A couple more tweaks to the nvim config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 vim.opt.relativenumber = false
 vim.go.background = "light"
+vim.g.snacks_animate = false
+
+local opt = vim.opt
+-- Use tabs, not spaces
+opt.expandtab = false
+-- 4 spaces for tab, not 2
+opt.tabstop = 4
